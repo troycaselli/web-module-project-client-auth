d4734936ac5585dba26dce6abba70b39e64af560
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-10-28 11:20:57 -0700

    created local state to show error message for incorrect password.

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,6 +12,7 @@ const Login = (props) => {
     let navigate = useNavigate();
 
     const [credentials, setCredentials] = useState(initialData);
+    const [error, setError] = useState('');
 
     const handleChange = (e) => {
         setCredentials({...credentials, [e.target.name]: e.target.value});
@@ -21,16 +22,21 @@ const Login = (props) => {
         e.preventDefault();
         axios.post('http://localhost:9000/api/login', credentials)
             .then(res => {
+                setError('');
                 localStorage.setItem('token', res.data.token);
                 props.handleToggleLoggedIn();
                 return navigate('/friends');
             })
-            .catch(err => console.log(err.response.data.error));
+            .catch(err => {
+                console.log(err.response.data.error);
+                setError(err.response.data.error);
+            });
     }
 
     return(
         <section id='login-wrapper'>
             <h1>LOGIN</h1>
+            <p id='error'>{error}</p>
             <form onSubmit={handleSubmit}>
                 <div className='input-wrapper'>
                     <p>USERNAME</p>
